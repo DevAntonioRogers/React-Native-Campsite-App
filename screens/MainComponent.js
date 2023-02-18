e15bdd51af6c45503496f675b7861bade929a57c
DevAntonioRogers <hydrohs6@gmail.com>
2023-02-18 12:16:00 -0500

    Added about and contact page

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -5,6 +5,8 @@ import DirectoryScreen from './DirectoryScreen';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import HomeScreen from './HomeScreen';
+import AboutScreen from './AboutScreen';
+import ContactScreen from './ContactScreen';
 
 const Drawer = createDrawerNavigator();
 
@@ -26,6 +28,7 @@ const HomeNavigator = () => {
   );
 };
 
+
 const DirectoryNavigator = () => {
   const Stack = createStackNavigator();
   return (
@@ -44,6 +47,31 @@ const DirectoryNavigator = () => {
         options={({ route }) => ({
           title: route.params.campsite.name
         })}
+      />
+    </Stack.Navigator>
+  );
+};
+
+const AboutNavigator = () => {
+  const Stack = createStackNavigator();
+  return (
+    <Stack.Navigator screenOptions={screenOptions}>
+      <Stack.Screen
+        name='About'
+        component={AboutScreen}
+      />
+    </Stack.Navigator>
+  );
+};
+
+const ContactNavigator = () => {
+  const Stack = createStackNavigator();
+  return (
+    <Stack.Navigator screenOptions={screenOptions}>
+      <Stack.Screen
+        name='Contact'
+        component={ContactScreen}
+        options={{ title: 'Contact Us' }}
       />
     </Stack.Navigator>
   );
@@ -71,6 +99,15 @@ const Main = () => {
           name='Directory'
           component={DirectoryNavigator}
           options={{ title: 'Directory' }}
+        />
+        <Drawer.Screen
+          name='About'
+          component={AboutNavigator}
+        />
+        <Drawer.Screen
+          name='Contact'
+          component={ContactNavigator}
+          options={{ title: 'Contact Us' }}
         />
       </Drawer.Navigator>
     </View>
